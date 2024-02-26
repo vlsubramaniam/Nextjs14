8dc63e136d63e417f60726030a21ce96e76b4770
vlsubramaniam <subramaniam.vls@gmail.com>
2024-02-26 18:22:13 +0530

    26-FEB-2024

--- 01-intro/app/page.js
+++ 01-intro/app/page.js
@@ -2,12 +2,11 @@ import Link from 'next/link';
 import React from 'react';
 
 const HomePage = () => {
-  console.log('Home Page'); // This will not be displayed on the screen as this is a server component
+  console.log('Home Page rendered'); // This will not be displayed on the console as this is a server component
   return (
     <div>
       <h1 className='text-5xl mb-8 font-bold'>Next.js Tutorial</h1>
       <Link href='/client' className='btn btn-accent'>
-        {' '}
         Get Started
       </Link>
     </div>
