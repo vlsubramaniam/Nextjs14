347fe9a5dedceee517292baff0a78aef1eb52122
vlsubramaniam <subramaniam.vls@gmail.com>
2024-03-05 08:42:06 +0530

    05-MAR-2024

--- property-pulse/app/layout.jsx
+++ property-pulse/app/layout.jsx
@@ -1,18 +1,20 @@
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'PropertyPulse | Find the Perfect Rental',
   description: 'Find your dream rental property',
-  keywords: 'rental, find rentals, find properties'
-}
+  keywords: 'rental, find rentals, find properties',
+};
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
