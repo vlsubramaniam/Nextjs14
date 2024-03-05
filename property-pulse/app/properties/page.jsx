8738424868ef918f329dfd7edaea852d86ef0777
vlsubramaniam <subramaniam.vls@gmail.com>
2024-03-05 09:33:42 +0530

    05-MAR-2024

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,12 +1,22 @@
-import Link from 'next/link';
+import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
 
 const PropertiesPage = () => {
   return (
-    <div>
-        <h1 className='text-3xl'>Properties</h1>
-        <Link href='/'>Go Home</Link>
-    </div>
-  )
-}
+    <section class='px-4 py-6'>
+      <div class='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div class='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 
-export default PropertiesPage
+export default PropertiesPage;
